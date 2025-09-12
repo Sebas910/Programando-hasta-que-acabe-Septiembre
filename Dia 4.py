d2bb9e7b688fe7b0f777fb51b65ceb2dd0dc114a
Sbxz <josequinterosebastian@gmail.com>
2025-09-11 22:22:32 -0300

    Dia 4 Diccionarios

---
+++ Dia 4.py
@@ -0,0 +1,18 @@
+texto = input("Ingrese un texto cualquier mi pana: ")
+
+lista_palabras = texto.split()
+
+print(lista_palabras)
+
+diccionario_palabras = {}
+
+for palabras in lista_palabras: #Increible que en python esto sea suficiente para verificar si hay algo dentro de una ED 
+    
+    if palabras in diccionario_palabras:
+        
+        diccionario_palabras[palabras] += 1
+    else:
+        diccionario_palabras[palabras] = 1
+    
+    
+print (diccionario_palabras)
