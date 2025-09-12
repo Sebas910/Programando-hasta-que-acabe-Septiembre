62d819b992f315e3fe010270d81514ecf14f634a
Sbxz <josequinterosebastian@gmail.com>
2025-09-11 22:29:49 -0300

    Mejorando aun mas el print

--- Dia 4.py
+++ Dia 4.py
@@ -2,8 +2,6 @@ texto = input("Ingrese un texto cualquier mi pana: ")
 
 lista_palabras = texto.split()
 
-print(lista_palabras)
-
 diccionario_palabras = {}
 
 for palabras in lista_palabras: #Increible que en python esto sea suficiente para verificar si hay algo dentro de una ED 
@@ -13,10 +11,11 @@ for palabras in lista_palabras: #Increible que en python esto sea suficiente par
         diccionario_palabras[palabras] += 1
     else:
         diccionario_palabras[palabras] = 1
-    
+
+print("Palabras Encontradas...")
 for clave, valor in diccionario_palabras.items():
     
-    print(clave, valor)
+    print(clave,":", valor)
     
    
     
