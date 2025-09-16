71187861ec71b062b2302a316f0d9a4d70b6d369
Sbxz <josequinterosebastian@gmail.com>
2025-09-16 10:17:16 -0300

    Hola mundo diferente

---
+++ Dia 8.py
@@ -0,0 +1,7 @@
+print()
+Persona = input("Ingrese su nombre: ")
+Edad = int(input("Ingrese su edad: "))
+
+print("hola" ," " ,Persona ,"su edad es " ,Edad) 
+
+print("pero dentro de 5 anos su edad sera: ", Edad+5)
