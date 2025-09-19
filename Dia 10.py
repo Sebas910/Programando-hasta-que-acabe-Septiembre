ef32687f6aba9b63f16dc28ef5dfae7452e6bcb5
Sbxz <josequinterosebastian@gmail.com>
2025-09-18 23:36:04 -0300

    Nada interesante

---
+++ Dia 10.py
@@ -0,0 +1,12 @@
+import random
+
+
+numero = random.randint(0,150)
+
+if numero != 0:
+    
+    print("No queria perder la racha :,)")
+    
+else:
+    
+    print("asi que si lees esto, no hay nada interesante aqui <3")
